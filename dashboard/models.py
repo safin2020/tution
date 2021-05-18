60fb793df24307879b285a4ec778721ec1bbf071
unknown <alsafin67@gmail.com>
2021-05-18 16:32:13 +0600

    update1

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,6 +7,6 @@ from django.db import models
 class JoiningClass(models.Model):
     joiningText = models.TextField(blank=True)
     noclasstext = models.TextField(blank=True)
-   
     classlink = models.URLField(max_length=200,blank=True)
+    noticeText = models.TextField(blank=True,null=True)
     active = models.BooleanField(default=False)
